348013728ae699c71e10b58c9200aaa744955345
Ronnie24 <ROnnie-wai@hotmail.com>
2020-01-09 22:22:30 +1300

    Update ExtentFactory.java

--- src/Utilities/ExtentFactory.java
+++ src/Utilities/ExtentFactory.java
@@ -1,14 +1,10 @@
 package Utilities;
-
 import com.relevantcodes.extentreports.ExtentReports;
-
 public class ExtentFactory {
 	public static ExtentReports getInstance(){
 		ExtentReports extent;
 		String path ="E:\\EW\\TestNGYahooLogin\\report-demo.html";
 		extent = new ExtentReports(path,false);
-		
 		return extent;
 	}
-
 }
