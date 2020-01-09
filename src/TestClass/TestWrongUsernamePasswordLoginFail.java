6715b8cb789b86d32f4806c1896bacd2f10ae614
Ronnie24 <ROnnie-wai@hotmail.com>
2020-01-09 22:18:07 +1300

    Update TestWrongUsernamePasswordLoginFail.java

--- src/TestClass/TestWrongUsernamePasswordLoginFail.java
+++ src/TestClass/TestWrongUsernamePasswordLoginFail.java
@@ -1,50 +1,40 @@
 package TestClass;
-
 import org.testng.annotations.Test;
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-
 import Utilities.Contanst;
 import Utilities.EmailPage;
 import Utilities.ExtentFactory;
 import Utilities.ScreenShot;
 import Utilities.UsernameAndPassword;
-
 import org.testng.annotations.BeforeMethod;
-
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-
+/**
+ * @throws InterruptedException
+ * Enter incorrect username, incorrect password and take screenShot if login failed
+ */
 public class TestWrongUsernamePasswordLoginFail {
 	private WebDriver driver;
 	ExtentReports report;
 	ExtentTest test;
-	/**
-	 * @throws InterruptedException
-	 * Enter incorrect username, correct password and take screenShot if login failed
-	 */
+
   @Test
   public void loginWithWrongUsernameAndPassword() throws InterruptedException {
 		//Enter Username
 		EmailPage.enterUsername(driver,"test");
 		test.log(LogStatus.INFO, "Enter username...");
-		
 		//Enter password
 		EmailPage.enterPassword(driver, "test");
 		test.log(LogStatus.INFO, "Enter password");
 		Thread.sleep(3000);
-		
-
-		
 		//Check login status
 		boolean loginStatus = EmailPage.isLoginSuccess(driver);
 		if( loginStatus == true){
@@ -54,6 +44,7 @@ public class TestWrongUsernamePasswordLoginFail {
 			test.log(LogStatus.FAIL,"login failed");
 		}
   }
+	
   @BeforeMethod
   public void beforeMethod() {
 		driver = new ChromeDriver();
@@ -63,15 +54,11 @@ public class TestWrongUsernamePasswordLoginFail {
 		// maximize window
 		driver.manage().window().maximize();
 		test.log(LogStatus.INFO, "maximize window");
-
 		// Waiting Page Loading
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
 		// Web Application Opened
 		driver.get(Contanst.baseUrl);
 		test.log(LogStatus.INFO, "Web Application Opened");
-
-
   }
 
   @AfterMethod
@@ -84,5 +71,4 @@ public class TestWrongUsernamePasswordLoginFail {
 		report.endTest(test);
 		report.flush();
   }
-
 }
