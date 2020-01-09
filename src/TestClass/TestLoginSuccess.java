4a41f940a38270844e15db8a00a12816504f32e7
Ronnie24 <ROnnie-wai@hotmail.com>
2020-01-09 22:15:04 +1300

    Update TestLoginSuccess.java

--- src/TestClass/TestLoginSuccess.java
+++ src/TestClass/TestLoginSuccess.java
@@ -1,53 +1,38 @@
 package TestClass;
-
 import org.testng.annotations.Test;
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-
 import Utilities.Contanst;
 import Utilities.EmailPage;
 import Utilities.ExtentFactory;
 import Utilities.UsernameAndPassword;
-
 import org.testng.annotations.BeforeMethod;
-
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-
 /**
  * @throws InterruptedException
  * Enter correct username, password
  * Upload attachment and send
  */
-
-
 public class TestLoginSuccess {
 	private WebDriver driver;
 	ExtentReports report;
 	ExtentTest test;
-
 	@Test
 	public void test1() throws InterruptedException, IOException {
 		//Enter Username
 		EmailPage.enterUsername(driver,UsernameAndPassword.user);
 		test.log(LogStatus.INFO, "Enter username...");
-		
-		
 		//Enter password
 		EmailPage.enterPassword(driver,UsernameAndPassword.pass);
 		test.log(LogStatus.INFO, "Enter password");
 		Thread.sleep(3000);
-		
-
-		
 		//Check login status
 		boolean loginStatus = EmailPage.isLoginSuccess(driver);
 		if( loginStatus == true){
@@ -56,27 +41,22 @@ public class TestLoginSuccess {
 		}else{
 			test.log(LogStatus.FAIL,"login failed");
 		}
-		
 		//New letter
 		EmailPage.clickNewLetter(driver);
 		test.log(LogStatus.INFO, "Click New Letter button");
 		Thread.sleep(3000);
-		
 		//Enter email address
 		EmailPage.enterEmailAddress(driver,"Ronnie-wai@hotmail.com");
-		test.log(LogStatus.INFO, "Enter email address");
-		
+		test.log(LogStatus.INFO, "Enter email address");		
 		//Click upload attachment button and upload attachment with AutiIt tool
 		EmailPage.uploadAttachment(driver);
 		Thread.sleep(2000);
 		Runtime.getRuntime().exec("E:\\EW\\TestNGYahooLogin\\script\\fileUpload.exe");
 		test.log(LogStatus.INFO, "Upload attachment");
-		Thread.sleep(2000);
-		
+		Thread.sleep(2000);		
 		//Click send email button
 		EmailPage.sendEmail(driver);
-		Thread.sleep(3000);
-		
+		Thread.sleep(3000);		
 		//Check is email send successfully
 		boolean isEmailSent = EmailPage.isEamilSent(driver);
 		if( isEmailSent == true){
@@ -85,7 +65,6 @@ public class TestLoginSuccess {
 		}else{
 			test.log(LogStatus.FAIL,"Email sent failed");
 		}
-
 	}
 
 	@BeforeMethod
@@ -97,10 +76,8 @@ public class TestLoginSuccess {
 		// maximize window
 		driver.manage().window().maximize();
 		test.log(LogStatus.INFO, "maximize window");
-
 		// Waiting Page Loading
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
 		// Web Application Opened
 		driver.get(Contanst.baseUrl);
 		test.log(LogStatus.INFO, "Web Application Opened");
@@ -113,5 +90,4 @@ public class TestLoginSuccess {
 		report.endTest(test);
 		report.flush();
 	}
-
 }
