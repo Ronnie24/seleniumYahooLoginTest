8542f4703460d235f4c8cdd82333495c2364542d
Ronnie24 <ROnnie-wai@hotmail.com>
2020-01-09 22:23:00 +1300

    Update ScreenShot.java

--- src/Utilities/ScreenShot.java
+++ src/Utilities/ScreenShot.java
@@ -1,8 +1,6 @@
 package Utilities;
-
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -15,8 +13,6 @@ public class ScreenShot {
 		File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(sourceFile, new File(dir+fileName));
 		String destination = dir + fileName;
-		return destination;
-		
+		return destination;		
 	}
-
 }
